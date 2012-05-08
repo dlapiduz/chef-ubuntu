6b5ad9a8fefd96b9181ec64acacb6cb073f0efc0
Diego Lapiduz <diego@sinaistudio.com>
2012-05-08 19:27:41 -0400

    compile ruby from source

--- setup.sh
+++ setup.sh
@@ -5,13 +5,15 @@ export DEBIAN_FRONTEND=noninteractive
 # update the apt library with the current versions
 apt-get --yes --quiet update
 
-apt-get --yes --quiet install build-essential zlib1g zlib1g-dev libreadline5 libreadline-gplv2-dev libssl-dev git
+apt-get --yes --quiet install build-essential zlib1g zlib1g-dev libreadline6 libreadline6-dev libssl-dev libyaml-dev git libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison
 
-curl -L get.rvm.io | bash -s stable
+cd /tmp && wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz
 
-source /etc/profile.d/rvm.sh
+tar xzvf ruby-1.9.3-p194.tar.gz
 
-rvm install 1.9.3-p125 --default
+cd /tmp/ruby-1.9.3-p194
+./configure --prefix=/usr/local --enable-shared --disable-install-doc --with-opt-dir=/usr/local/lib
+make && make install
 
 gem install chef ohai ruby-shadow --no-ri --no-rdoc
 
