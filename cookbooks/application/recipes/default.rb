9687d0c4f94146153c87eff504004c9f04b35528
Diego Lapiduz <diego@lapiduz.com>
2012-05-10 22:29:42 +0000

    remove include rails

--- cookbooks/application/recipes/default.rb
+++ cookbooks/application/recipes/default.rb
@@ -16,8 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_recipe "application::rails"
-
 application node[:app][:id] do
   path node[:app][:deploy_to]
   owner "www-data"
